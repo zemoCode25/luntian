936ff170731f972233313751df7d6b279ecaa333
Admin <ralphbryancarlos27@gmail.com>
2025-08-11 23:00:19 +0800

    feat: border color

--- src/app/(home)/components/Header.tsx
+++ src/app/(home)/components/Header.tsx
@@ -5,31 +5,33 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex p-8 w-full justify-between dark:bg-green-900">
-      <Link href={"/"}>
-        <span className="flex justify-between gap-2">
-          <PawPrint color="#568d6f" />
-          <span className="text-lg font-bold">PetVision</span>
-        </span>
-      </Link>
-      <ul className="flex justify-between gap-4">
+    <header className="flex items-center justify-center dark:bg-green-900 border-b border-gray-b-300 dark:border-bgray-600">
+      <section className="flex px-8 py-4 w-full max-w-[1280px] justify-between items-center">
         <Link href={"/"}>
-          <li className="px-4 py-2 hover:bg-accent hover:text-gray-100 rounded-md cursor-pointer">
-            Home
-          </li>
+          <span className="flex justify-between gap-2">
+            <PawPrint color="#568d6f" />
+            <span className="text-lg font-bold">PetVision</span>
+          </span>
         </Link>
-        <Link href={"/about-us"}>
-          <li className="px-4 py-2 hover:bg-accent hover:text-gray-100 rounded-md cursor-pointer">
-            About Us
-          </li>
-        </Link>
-        <Link href={"/queries"}>
-          <li className="px-4 py-2 hover:bg-accent hover:text-gray-100 rounded-md cursor-pointer">
-            Queries
-          </li>
-        </Link>
-      </ul>
-      <ThemeModeToggle />
+        <ul className="flex justify-between gap-4">
+          <Link href={"/"}>
+            <li className="px-4 py-2 hover:bg-accent hover:text-gray-100 rounded-md cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link href={"/about-us"}>
+            <li className="px-4 py-2 hover:bg-accent hover:text-gray-100 rounded-md cursor-pointer">
+              About Us
+            </li>
+          </Link>
+          <Link href={"/queries"}>
+            <li className="px-4 py-2 hover:bg-accent hover:text-gray-100 rounded-md cursor-pointer">
+              Queries
+            </li>
+          </Link>
+        </ul>
+        <ThemeModeToggle />
+      </section>
     </header>
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="w-[80rem] mx-auto bg-main">
+    <div className="bg-main">
       <Header />
     </div>
   );
