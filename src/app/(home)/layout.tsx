6e7ab34dbe9a7d118241d5b2e29ce6ce0a379bda
Admin <ralphbryancarlos27@gmail.com>
2025-08-11 14:06:03 +0800

    chore: add groups folder for main pages

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../global.css";
+import { Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} antialiased`}
       >
         {children}
       </body>
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div><Link href={"/dashboard"}>Dashboard!</Link></div>
+  )
+}
