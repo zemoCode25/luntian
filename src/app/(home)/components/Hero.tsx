487500f0182f626e30f32431e810771ce8ddd92c
Admin <ralphbryancarlos27@gmail.com>
2025-08-12 14:14:07 +0800

    refactor: fix header and section height

--- src/app/(home)/components/Hero.tsx
+++ src/app/(home)/components/Hero.tsx
@@ -4,13 +4,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col gap-5 lg:flex-row px-8 py-4 w-full max-w-[1280px] h-[30rem] mx-auto justify-between items-center">
+    <section className="flex flex-col gap-2 lg:flex-row px-8 py-4 w-full h-[calc(100dvh-4.6rem)] max-w-[1280px] mx-auto justify-between items-center">
       <div className="flex flex-col gap-2 items-center lg:items-baseline">
         <h1 className="text-accent dark:text-main text-4xl font-bold text-center md:text-6xl lg:text-left">
           Detect Animal <br />
           Diseases in Seconds
         </h1>
-        <p className="w-full max-w-[30rem] dark:text-gray-300 text-center lg:text-left">
+        <p className="w-full text-gray-600 max-w-[30rem] dark:text-gray-300 text-center lg:text-left text-sm lg:text-base">
           Harness the power of AI to identify potential diseases in pets or
           livestock. Simply upload a photo, get instant diagnosis.
         </p>
@@ -19,7 +19,7 @@ export default function Hero() {
         </Button>
       </div>
       <div>
-        <div className="w-[100%] lg:min-w-[30rem] lg:h-[300px] min-w-[25rem] h-[13rem] relative">
+        <div className="vw- lg:min-w-[30rem] lg:h-[300px] min-w-[25rem] h-[13rem] relative">
           <Image
             src="/images/rabbit_consulted.jpg"
             fill
