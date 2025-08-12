693d05331878362057b419dcb1214ce7e28111e9
Admin <ralphbryancarlos27@gmail.com>
2025-08-12 12:49:16 +0800

    feat: hero section mobile responsiveness

--- src/app/(home)/components/Hero.tsx
+++ src/app/(home)/components/Hero.tsx
@@ -4,29 +4,29 @@ import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
-    <section className="flex px-8 py-4 w-full max-w-[1280px] h-[30rem] mx-auto justify-between items-center">
-      <div className="flex flex-col gap-2">
-        <h1 className="text-accent dark:text-main text-6xl font-bold">
+    <section className="flex flex-col gap-5 lg:flex-row px-8 py-4 w-full max-w-[1280px] h-[30rem] mx-auto justify-between items-center">
+      <div className="flex flex-col gap-2 items-center lg:items-baseline">
+        <h1 className="text-accent dark:text-main text-4xl font-bold text-center md:text-6xl lg:text-left">
           Detect Animal <br />
           Diseases in Seconds
         </h1>
-        <p className="w-full max-w-[35rem] dark:text-gray-300">
-          Harness the power of AI to quickly identify potential diseases in pets
-          or livestock. Simply upload a photo, get instant diagnosis.
+        <p className="w-full max-w-[30rem] dark:text-gray-300 text-center lg:text-left">
+          Harness the power of AI to identify potential diseases in pets or
+          livestock. Simply upload a photo, get instant diagnosis.
         </p>
         <Button className="w-fit bg-accent py-6 px-8 hover:bg-green-700 cursor-pointer">
           Get Started
         </Button>
       </div>
       <div>
-        <span className="w-[90%] min-w-[20rem] h-[300px] relative">
+        <div className="w-[100%] lg:min-w-[30rem] lg:h-[300px] min-w-[25rem] h-[13rem] relative">
           <Image
-            src="/images/dog_consulted.webp"
-            fill={true}
+            src="/images/rabbit_consulted.jpg"
+            fill
             alt="Description"
-            className="object-contain"
+            className="object-cover rounded-3xl shadow-xl shadow-green-800/10"
           />
-        </span>
+        </div>
       </div>
     </section>
   );
