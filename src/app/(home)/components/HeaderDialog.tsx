e4fec0195c1e00cdb4caf136c8c9aee23203dea0
Admin <ralphbryancarlos27@gmail.com>
2025-08-13 09:17:16 +0800

    feat: create header dialog UI

--- src/app/(home)/components/HeaderDialog.tsx
+++ src/app/(home)/components/HeaderDialog.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import {
+  ModeToggle,
+  ModeToggle as ThemeModeToggle,
+} from "@/components/utils/ThemeModeToggle";
+import Link from "next/link";
+import { X } from "lucide-react";
 
 export default function HeaderDialog() {
   return (
-    <div>
-      <p>Show Dialog</p>
-    </div>
+    <header className="fixed top-0 bottom-0 left-0 right-0 flex justify-between z-50 bg-main p-5 dark:bg-green-950">
+      <div>
+        <ThemeModeToggle />
+      </div>
+
+      <ul className="flex flex-col justify-between gap-4 h-[30%] items-center">
+        <Link href={"/"}>
+          <li className="px-4 py-2 hover:bg-accent hover:text-gray-100 rounded-md cursor-pointer">
+            Home
+          </li>
+        </Link>
+        <Link href={"/about-us"}>
+          <li className="px-4 py-2 hover:bg-accent hover:text-gray-100 rounded-md cursor-pointer">
+            About Us
+          </li>
+        </Link>
+        <Link href={"/queries"}>
+          <li className="px-4 py-2 hover:bg-accent hover:text-gray-100 rounded-md cursor-pointer">
+            Queries
+          </li>
+        </Link>
+      </ul>
+      <div>
+        <X />
+      </div>
+    </header>
   );
 }
